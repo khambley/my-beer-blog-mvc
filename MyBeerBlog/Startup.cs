cc1226781dc90ed9020b1629976018c27bad08be
Katherine Hambley <superdesigngirl@mac.com>
2017-03-08 18:45:11 -0600

    Add project files.

---
+++ MyBeerBlog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyBeerBlog.Startup))]
+namespace MyBeerBlog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
