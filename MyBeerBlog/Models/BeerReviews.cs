d9b10c4afe37f900f1ac7362abfa4e21cabd1313
Katherine Hambley <superdesigngirl@mac.com>
2017-03-09 09:36:53 -0600

    added reviews controller and view

---
+++ MyBeerBlog/Models/BeerReviews.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyBeerBlog.Models
+{
+    public class BeerReviews
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Rating { get; set; }
+        public string Comments { get; set; }
+
+
+    }
+}
